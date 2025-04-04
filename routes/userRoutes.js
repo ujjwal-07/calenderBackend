29e6b5662049b37c2ca47d27dd4af85332b91bd3
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-04-05 01:26:15 +0530

    Update userRoutes.js
    added_excel_data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { addEvent, getEventsByDate, deleteEvent ,get,getEvents} = require("../controllers/userController");
+const multer = require('multer');
+const XLSX = require('xlsx');
+
+const { addEvent, getEventsByDate, deleteEvent ,get,getEvents,addExcelData} = require("../controllers/userController");
+const { Upload } = require("lucide-react");
 
 const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 router.post("/events", addEvent);
 router.get("/get", get);
 router.get("/getEvents",getEvents)
 router.get("/event/:formattedDate", getEventsByDate);
 router.post("/deleteEvents", deleteEvent);
-
+router.post("/store_excel_data",upload.single("excelFile"), addExcelData)
 module.exports = router;
