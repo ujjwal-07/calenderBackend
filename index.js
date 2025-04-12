4e5703fed625b6a09e9098d7964432df7d0dd720
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-04-12 14:22:03 +0530

    Added console.log for learning purpose

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log("Hello this is added for learning purpose please ignore")
 app.use("/api", eventRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
